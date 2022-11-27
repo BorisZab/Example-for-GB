5f5f0fcb5b41664ef416a24044467f822ac71a65
BorisZ <boriszab1@yandex.ru>
2022-11-27 15:21:03 +0300

    add example2

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -7,11 +7,11 @@ while (n < 1 || n > 7)
     n = Convert.ToInt32(Console.ReadLine());
 }
 if (n == 1) Console.Write("Понедельник");
-if (n == 2) Console.Write("Вторник");
-if (n == 3) Console.Write("среда");
-if (n == 4) Console.Write("четверг");
-if (n == 5) Console.Write("пятница");
-if (n == 6) Console.Write("суббота");
+else if (n == 2) Console.Write("Вторник");
+else if (n == 3) Console.Write("среда");
+else if (n == 4) Console.Write("четверг");
+else if (n == 5) Console.Write("пятница");
+else if (n == 6) Console.Write("суббота");
 if (n == 7) Console.Write("воскресенье");
 
 
---
+++ Example002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int i = n * (-1);
+// while (i <= n) // созадли цикл от - i до n
+// {
+//     Console.Write($"{i} ");// вывод переменой i
+//     i = i + 1; //переход к следующему числу
+// }
+
+//Второе решение
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = (-1) * n; i<=n; i++ )
+Console.Write( i + " ");
