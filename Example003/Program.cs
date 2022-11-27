9698d9258dbcf93517dab8a2fc68067d3f925d13
BorisZ <boriszab1@yandex.ru>
2022-11-27 18:37:56 +0300

    new save

---
+++ Example003/Program.cs
@@ -0,0 +1,6 @@
+﻿// узнать последнию цифру любого числа 
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(n % 10); // деление на остаток 
