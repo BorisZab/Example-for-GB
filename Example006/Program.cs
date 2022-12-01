84e1eae0c31832db334a38f92006be145f98c37a
BorisZ <boriszab1@yandex.ru>
2022-12-01 17:25:43 +0300

    add example006

---
+++ Example006/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b % a == 0) 
+Console.WriteLine("Кратно");
+else
+Console.WriteLine($"Не кратно {a % b}");
