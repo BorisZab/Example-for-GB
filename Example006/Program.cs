c93b15ee622f74e7d8182229b22a7645b59d39d0
BorisZ <boriszab1@yandex.ru>
2022-12-01 17:34:31 +0300

    add example007

--- Example006/Program.cs
+++ Example006/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear();
+﻿// пользователь вводит два числа если одно кратно другому, то программа выодит "кратно",
+// если нет от ввыодит остаток при делении.
+Console.Clear();
 Console.WriteLine("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
---
+++ Example007/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа принимает на вход число и проверяет, кратно ли оно одовременно 7 и 23
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 7 == 0 & a % 23 == 0) 
+Console.WriteLine("yes");
+else
+Console.WriteLine("no");
