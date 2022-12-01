86b5d71f157803df0e1c9d77708caa1b7a90f2d2
BorisZ <boriszab1@yandex.ru>
2022-12-01 17:42:20 +0300

    add example008

---
+++ Example008/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа на вход принимает 2 числа и проверяет 1 или 2 являеться квадратом другого
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b * b == a || a * a == b)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
