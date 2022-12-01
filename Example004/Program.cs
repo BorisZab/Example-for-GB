50c373553b8a5d9a60eab74a35d574f11bf2e729
BorisZ <boriszab1@yandex.ru>
2022-12-01 16:32:20 +0300

    add example 004

---
+++ Example004/Program.cs
@@ -0,0 +1,11 @@
+﻿// нахождение  двух случайных чисел  в отрезке от 10 до 99 и нахождение больших.
+
+Console.Clear();
+int n = new Random().Next(10, 100);
+int n1= n / 10;
+int n2 = n % 10;
+Console.WriteLine($"Случайное число: {n}");
+if (n1>n2)
+Console.Write(n1);
+else
+Console.Write(n2);
