50e74980904ef75a7c8dceb62535595c9f996c78
BorisZ <boriszab1@yandex.ru>
2022-12-03 18:22:25 +0300

    add example010

---
+++ Example010/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите четверть  x: ");
+double x = Convert.ToInt32(Console.ReadLine());
+
+
+if (x == 1)
+    Console.WriteLine("x > 0 && y > 0");
+else if (x == 3)
+    Console.WriteLine("x < 0 && y > 0");
+else if (x == 3)
+    Console.WriteLine("x < 0 && y < 0");
+else if (x == 4)
+    Console.WriteLine("x < 0 && y > 0");
