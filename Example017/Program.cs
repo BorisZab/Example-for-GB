12538fae64da4f6b1aac5755155ab5001d5d467a
BorisZ <boriszab1@yandex.ru>
2022-12-11 18:36:52 +0300

    add example 017

---
+++ Example017/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите програумму которая принимает на число и выдвает колличество цифр в числе
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (n / 10 != 0)
+{
+    n /= 10;
+    count++;
+}
+Console.WriteLine(count);
