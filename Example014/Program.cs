4e5b524f8380d2f956b4aa79179fb821339cd9a2
BorisZ <boriszab1@yandex.ru>
2022-12-11 17:46:52 +0300

    add example 014

---
+++ Example014/Program.cs
@@ -0,0 +1,12 @@
+﻿// Ввод масива от пользователя 
+Console.Clear();
+Console.Write("Введите колличетсво элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент { i +1} массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+// выводит на экран массив и разделяет его , и пробелом 
+Console.WriteLine($"[{string.Join(", ", array)}]"); 
