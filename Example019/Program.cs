a5fb99e81355b7480b7989763f6e5166f55ec143
BorisZ <boriszab1@yandex.ru>
2022-12-11 20:12:29 +0300

    add exampale 019

---
+++ Example019/Program.cs
@@ -0,0 +1,13 @@
+﻿// проверка являеться ли число полиндромом 12321
+Console.Clear();
+Console.Write("Введите число: ");
+string? n = Console.ReadLine();
+int newNumber = 0;
+// переворачивает число 
+for (int i = n.Length - 1; i >= 0; i--)
+    newNumber = newNumber + Convert.ToInt32(int.Parse(n[i].ToString()) * Math.Pow(10, i));
+Console.WriteLine(newNumber);
+if (newNumber == int.Parse(n))
+Console.WriteLine("yes");
+else
+Console.WriteLine("no");   
