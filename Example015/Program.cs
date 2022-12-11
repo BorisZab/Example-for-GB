066d4e461d2a14a6c8fb9c495350aab7b8950afa
BorisZ <boriszab1@yandex.ru>
2022-12-11 18:13:40 +0300

    add example 015

---
+++ Example015/Program.cs
@@ -0,0 +1,30 @@
+﻿// Найти максиму из двух чилес 
+// int numberMax(int n, int m)
+// {
+//     if (n > m)
+//         return n;
+//     return m;
+// }
+
+// Console.Clear();
+// Console.Write("Введите первое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(numberMax(n, m));
+
+
+
+
+
+
+void numberMax(ref int n)
+{
+    n = n + 10;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+numberMax(ref n);
+Console.WriteLine(n);
